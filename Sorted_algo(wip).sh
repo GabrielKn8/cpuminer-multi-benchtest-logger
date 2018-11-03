0b63dd08506496871a729fda7b37b60e14020456
GabrielKn8 <25245883+GabrielKn8@users.noreply.github.com>
2018-11-03 20:26:05 +0600

    Update Sorted_algo(wip).sh

--- Sorted_algo(wip).sh
+++ Sorted_algo(wip).sh
@@ -7,8 +7,7 @@ TIME=120
 mkdir -p $DIR
 touch $DIR/comp.txt
 
-for algo in scrypt scrypt-jane sha256d axiom bastion blake blake2s bmw cryptonight cryptonight-light decred dmd-gr fresh goestl lbry lyra2RE lyra2REv2 myr-gr neoscrypt nist5 pentablake  pluck quark qubit skein skein2 s3 sia sib timetravel vanilla veltor x11evo x11 x13 x14 x15 x17 yescrypt zr5 m7m keccak luffa shavite3 xevan
-
+for algo in scrypt scrypt:N scrypt-jane:N sha256d allium axiom bitcore blake blakecoin blake2s bmw cryptonight cryptolight decred dmd-gr drop fresh groestl heavy jha keccak keccakc luffa lyra2re lyra2rev2 myr-gr neoscrypt nist5 pentablake pluck phi phi2 quark qubit rainforest shavite3 sonoa skein skein2 s3 sia sib timetravel vanilla x11evo x11 x13 x14 x15 x16r x16s x17 yescrypt zr5 xevan
 do
 
 timeout $TIME ./cpuminer --benchmark -a $algo >> $DIR/$algo.log
